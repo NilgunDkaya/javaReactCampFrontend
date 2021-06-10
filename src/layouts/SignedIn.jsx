31cdb7ced2963f9e93384d18eab470f09e865a41
NilgunDkaya <61917558+NilgunDkaya@users.noreply.github.com>
2021-06-10 19:27:01 +0300

    Add files via upload

---
+++ src/layouts/SignedIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Dropdown, Menu, Image } from 'semantic-ui-react'
+
+export default function SignedIn({signOut}) {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://picsum.photos/id/106/2592/1728" />
+                <Dropdown pointing="top left" text="Nilgün">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+
+        </div>
+    )
+}
